25fe02685c8d616bb052d0f326ce20ce9c4ef073
zunzun <zunzun@zunzun.com>
2016-12-17 12:17:09 -0600

    Added check for Python 2 or Python 3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
 
-
-
-
 #    pyeq3 is a collection of equations expressed as Python classes
 #
 #    Copyright (C) 2013 James R. Phillips
@@ -11,6 +8,11 @@
 #    email: zunzun@zunzun.com
 #
 #    License: BSD-style (see LICENSE.txt in main source directory)
+
+import sys
+ver = sys.version_info[0]
+if ver < 3:
+    raise Exception('Python 3 is required to use pyeq3')
 
 from . import DataCache
 from . import Services
